51067a553b15c3e2e3eeddef300af72c20dd6273
Chrisfaherty <chrismactom@gmail.com>
2023-02-02 15:00:41 +0000

    Create and hide a new secret key

--- passwordvault/settings.py
+++ passwordvault/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    inport env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nm6xlpa^tlmz!2t^4+2y0f0^yp3f=k3q!oqn^1(rdfrc8_u%=5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
