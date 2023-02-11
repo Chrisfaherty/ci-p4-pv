3c13eaa028ef0648cf3158f8877b198e4b7a031b
Chrisfaherty <chrismactom@gmail.com>
2023-02-11 18:05:55 +0000

    Create a model for the contact form

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def contact_view(request):
-    return HttpResponse("Contact app works!")
+    return render(request, "contact.html")
 
 
