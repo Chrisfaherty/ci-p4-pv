224e4febd2929be912ecb44c860b29374f2b4dbc
Chrisfaherty <chrismactom@gmail.com>
2023-02-14 13:17:23 +0000

    Create the first models for the passwordmanager

--- passwordmanager/models.py
+++ passwordmanager/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Password(models.Model):
+    name = models.CharField(max_length =50, null=False, blank=False)
+    website = models.URLField()
+    email = models.EmailField()
+    username = models.CharField(max_length =50, null=False, blank=False)
+    password = models.CharField(max_length =50, null=False, blank=False)
+    message = models.TextField(max_length =250)
