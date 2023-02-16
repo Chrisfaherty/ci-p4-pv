0d9d85c7771b656ee4e916b623666d3cfd682674
Chrisfaherty <chrismactom@gmail.com>
2023-02-16 15:31:18 +0000

    Create a test file for the views.py functions

--- passwordmanager/tests_views.py
+++ passwordmanager/tests_views.py
@@ -1,8 +1,37 @@
 from django.test import TestCase
+from .models import Password
 
 
-class TestDjango(TestCase):
+class TestViews(TestCase):
 
 
-    def test_this_thing_works(self):
-        self.assertEqual(1, 0)
+    def test_get_passmanager(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'passmanager.html')
+
+    def test_get_add_password_page(self):
+        response = self.client.get('/add')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'add_password.html')
+
+    def test_get_edit_password_page(self):
+        passwords = Password.objects.create(name='Test password')
+        response = self.client.get(f'/edit/{password.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'edit_password.html')
+    
+    def test_can_add_password(self):
+        response = self.client.post('/add', {'name': 'Test Added Password'})
+        self.assertRedirect(response, '/')
+
+
+    def test_can_delete_item(self):
+        passwords = Password.objects.create(name='Test password')
+        response = self.client.get(f'/delete/{password.id}')
+        self.assertRedirect(response, '/')
+        exsisting_passwords = Password.objects.filter(id=password.id)
+        self.assertEqual(len(existing_passwords),0)
+
+
+        
