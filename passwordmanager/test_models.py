f68eda5a1a2be0b952f145a33478587e3748addc
Chrisfaherty <chrismactom@gmail.com>
2023-02-16 15:37:30 +0000

    Create a test to test the functions in the models.py file

--- passwordmanager/test_models.py
+++ passwordmanager/test_models.py
@@ -1,1 +1,9 @@
 from django.test import TestCase
+from .models import Password
+
+
+class TestModels(TestCase):
+
+    def test_done_defaults_to_false(self):
+        passwords = Password.objects.create(name='Test Passwords')
+        self.assertFalse(password.done)
